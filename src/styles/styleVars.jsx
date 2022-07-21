226137e0b274a5093d889d3be42fc13dda4c3f4f
MariaShebeko <marygro89@gmail.com>
2022-07-21 11:21:02 +0200

    created size and colors variables

---
+++ src/styles/styleVars.jsx
@@ -0,0 +1,14 @@
+//breakpoints
+export const size = {
+  mobile: '@media screen and (min-width: 320px)',
+  mobileMax: '@media screen and (max-width: 767px)',
+  tablet: '@media screen and (min-width: 768px)',
+  tabletMax: '@media screen and (max-width: 1279px)',
+  desktop: '@media screen and (min-width: 1280px)',
+};
+
+// Colors
+export const accentBgCl = '#996B60';
+export const textCl = '#000000';
+export const accentTextCl = '#ffffff';
+export const btnCl = '#191919';
