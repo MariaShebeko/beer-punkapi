0c517b40dc61cbd10019145ecd75220ec85b1d82
MariaShebeko <marygro89@gmail.com>
2022-07-22 16:09:06 +0200

    set pagination for switching prev and next list of beers.

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,55 @@
+import { useState, useEffect } from 'react';
+import api from '../../services/punkapi';
+import Container from '../../components/Container';
+import BeerList from '../../components/BeerList';
+import Pagination from '../../components/Pagination';
+import styled from 'styled-components';
+
+import { accentBgCl } from '../../styles/styleVars';
+
 export default function HomePage() {
+  const [beers, setBeers] = useState([]);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    api.getBeers(page).then(setBeers);
+  }, [page]);
+
+  // Уточнить как вытянуть totalPages чтобы дисэйблить кнопку пагинации
+
+  const onNextBtnClick = () => {
+    setPage(state => state + 1);
+    window.scrollTo({
+      behavior: 'smooth',
+      top: 0,
+    });
+  };
+  const onPrevBtnClick = () => {
+    setPage(state => state - 1);
+    window.scrollTo({
+      behavior: 'smooth',
+      top: 0,
+    });
+  };
+
   return (
-    <>
-      <h1>THIS IS A HOME PAGE</h1>
-    </>
+    <ContainerWrap>
+      <Container>
+        {beers && (
+          <>
+            <BeerList beers={beers} />
+            <Pagination prev={onPrevBtnClick} next={onNextBtnClick} />
+          </>
+        )}
+      </Container>
+    </ContainerWrap>
   );
 }
+
+const ContainerWrap = styled.div`
+  background-color: ${accentBgCl};
+`;
+
+// const DecorativePicture = styled.div`
+//   background-image: url('../../images/mainImage.jpg');
+// `;
