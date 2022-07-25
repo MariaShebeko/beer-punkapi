f9eef77bf1f2d5ff647c45cc24a790f3c514c746
MariaShebeko <marygro89@gmail.com>
2022-07-25 14:54:57 +0200

    added styles to Navigation component

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,14 +1,37 @@
 import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+import { textCl, accentTextCl, accentBgCl } from '../../styles/styleVars';
 
 const Navigation = () => {
   return (
-    <nav>
-      <NavLink to="/" exact>
-        Home
-      </NavLink>
-      <NavLink to="/beers">Beers</NavLink>
-    </nav>
+    <Nav>
+      <Link to="/">Home</Link>
+      <Link to="/beers">Beers</Link>
+    </Nav>
   );
 };
 
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-around;
+  text-align: center;
+  padding: 20px;
+
+  font-weight: 400;
+  font-size: 30px;
+  line-height: 1.5;
+
+  background-color: ${textCl};
+  box-shadow: 0px 2.98256px 7.4564px rgba(0, 0, 0, 0.1); ;
+`;
+
+const Link = styled(NavLink)`
+  text-decoration: none;
+  color: ${accentTextCl};
+  &.active,
+  &:hover {
+    color: ${accentBgCl};
+    transition: color 300ms ease-in-out;
+  }
+`;
 export default Navigation;
