b10386dabeeca8e4a8b680e97c02a394454e2041
MariaShebeko <marygro89@gmail.com>
2022-07-27 14:10:19 +0200

    added beer card render by id, created goback button to return on homepage

--- src/pages/BeerDetailPage/BeerDetailPage.jsx
+++ src/pages/BeerDetailPage/BeerDetailPage.jsx
@@ -1,3 +1,39 @@
+import { useState, useEffect } from 'react';
+import { useLocation, useParams, Link } from 'react-router-dom';
+import api from '../../services/punkapi';
+import BeerDetails from '../../components/BeerDetails';
+
 export default function BeerDetailPage() {
-  return <>THIS IS A BEER DETAIL PAGE</>;
+  const location = useLocation();
+  const params = useParams();
+
+  const beerId = Number(params.beerId);
+
+  console.log('location details', location);
+  // console.log('params details', params);
+
+  const [beer, setBeer] = useState(null);
+
+  useEffect(() => {
+    const asyncFetch = async () => {
+      const beer = await api.getBeerById(beerId);
+      setBeer(beer);
+    };
+    asyncFetch();
+  }, [beerId]);
+
+  return (
+    <>
+      {/* Нормально ли так хардкодить кнопку назад, до этого использовала хук useHistory */}
+      <Link
+        to={{
+          pathname: `/`,
+          state: { from: location },
+        }}
+      >
+        <button>Go back</button>
+      </Link>
+      {beer && <BeerDetails beer={beer} />}
+    </>
+  );
 }
