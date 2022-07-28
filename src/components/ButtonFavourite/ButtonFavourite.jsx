095aaa876ed4316102692a45f34417319d7d0099
MariaShebeko <marygro89@gmail.com>
2022-07-28 11:31:36 +0200

    added ButtonFavourite component

---
+++ src/components/ButtonFavourite/ButtonFavourite.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import { deleteFavourite, addFavourite } from '../../services/favourites';
+
+export default function ButtonFavourite({ beer }) {
+  const [inFav, setInFav] = useState(false);
+
+  const onFavouriteBtnClick = () => {
+    if (inFav) deleteFavourite(beer.id);
+    else addFavourite(beer);
+    setInFav(prevState => !prevState);
+  };
+
+  return (
+    <button type="button" onClick={onFavouriteBtnClick}>
+      {inFav ? 'Delete from favourites' : 'Add to favourites'}
+    </button>
+  );
+}
