be6be53b6e13ac97a1add5f373ba0d3376e62aee
MariaShebeko <marygro89@gmail.com>
2022-07-28 11:31:10 +0200

    added helper functions to add, remove and get favourite beers

---
+++ src/services/favourites.js
@@ -0,0 +1,23 @@
+const NAME = 'favourites';
+
+export const getFavourites = () => {
+  return JSON.parse(localStorage.getItem(NAME) || '[]');
+};
+
+export const addFavourite = beer => {
+  const fav = getFavourites();
+  if (fav.find(item => item.id === beer.id)) {
+    return;
+  }
+  fav.push(beer);
+  localStorage.setItem(NAME, JSON.stringify(fav));
+};
+
+export const deleteFavourite = id => {
+  const fav = getFavourites().filter(beer => beer.id !== id);
+  localStorage.setItem(NAME, JSON.stringify(fav));
+};
+
+export const inFavourites = id => {
+  return getFavourites().find(beer => beer.id === id);
+};
