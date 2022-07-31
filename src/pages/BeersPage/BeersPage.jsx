73680e0c129e80a026f62876d366d07f2fe2feba
MariaShebeko <marygro89@gmail.com>
2022-07-31 10:15:07 +0200

    added Searching beer by name at BeersPage

--- src/pages/BeersPage/BeersPage.jsx
+++ src/pages/BeersPage/BeersPage.jsx
@@ -1,3 +1,50 @@
+import { useState, useEffect } from 'react';
+import api from '../../services/punkapi';
+import SearchForm from '../../components/SearchForm';
+import Container from '../../components/Container';
+import BeerList from '../../components/BeerList';
+import Pagination from '../../components/Pagination';
+
 export default function BeersPage() {
-  return <>THIS IS A BEERS PAGE</>;
+  const [beers, setBeers] = useState([]);
+  const [beerName, setBeerName] = useState('');
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    if (beerName) {
+      api.getBeerByQuery(beerName, page).then(setBeers);
+    }
+  }, [beerName, page]);
+
+  const formSubmit = beerName => {
+    setBeerName(beerName);
+    setBeers([]);
+  };
+
+  const onNextBtnClick = () => {
+    setPage(state => state + 1);
+    window.scrollTo({
+      behavior: 'smooth',
+      top: 0,
+    });
+  };
+  const onPrevBtnClick = () => {
+    setPage(state => state - 1);
+    window.scrollTo({
+      behavior: 'smooth',
+      top: 0,
+    });
+  };
+
+  return (
+    <>
+      <Container>
+        <SearchForm onSubmit={formSubmit} />
+        {beers && <BeerList beers={beers} />}
+        {beers.length > 1 && (
+          <Pagination prev={onPrevBtnClick} next={onNextBtnClick} />
+        )}
+      </Container>
+    </>
+  );
 }
