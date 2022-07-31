cb8da324599980043d95b30a2c81069e04b0e776
MariaShebeko <marygro89@gmail.com>
2022-07-31 11:29:10 +0200

    added styles to search form

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react';
+import { BiSearchAlt } from 'react-icons/bi';
+import { accentBgCl, textCl } from '../../styles/styleVars';
+import styled from 'styled-components';
 
 export default function SearchForm({ onSubmit }) {
   const [beerName, setBeerName] = useState('');
@@ -20,19 +23,85 @@ export default function SearchForm({ onSubmit }) {
 
   return (
     <>
-      {
-        <form onSubmit={handleNameSubmit}>
-          <button type="submit">search</button>
-          <input
-            onChange={handleNameChange}
-            value={beerName}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Enter the name of the beer"
-          />
-        </form>
-      }
+      <Form onSubmit={handleNameSubmit}>
+        <Input
+          onChange={handleNameChange}
+          value={beerName}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search beer"
+        />
+        <Button type="submit">
+          <Icon />
+        </Button>
+      </Form>
     </>
   );
 }
+
+const Form = styled.form`
+  position: relative;
+
+  width: 400px;
+  margin: 0 auto 40px;
+`;
+const Button = styled.button`
+  position: absolute;
+  height: 50px;
+  font-size: 18px;
+  top: 0;
+  right: 0;
+
+  display: none;
+
+  min-width: 80px;
+
+  cursor: pointer;
+  transition: filter 0.3s cubic-bezier(0.3, 0.5, 0.4, 0.9);
+
+  border-radius: 0 10px 10px 0;
+  background: ${textCl};
+
+  font-weight: bold;
+  &:hover {
+    filter: brightness(0.8);
+  }
+`;
+const Input = styled.input`
+  position: relative;
+  height: 50px;
+  color: #2f2f2f;
+  font-size: 18px;
+
+  width: 100%;
+  padding: 0 15px;
+
+  transition: all 0.3s cubic-bezier(0.3, 0.5, 0.4, 0.9);
+  transition-property: width, border-radius;
+
+  border-radius: 10px;
+  outline: 0;
+
+  font-weight: 700;
+  appearance: none;
+  &:focus::placeholder {
+    color: ${accentBgCl};
+  }
+  &:not(:placeholder-shown) + button {
+    display: block;
+  }
+`;
+const Icon = styled(BiSearchAlt)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+
+  width: 30px;
+  height: 30px;
+  padding: 0;
+
+  transform: translate(-50%, -50%);
+
+  fill: ${accentBgCl};
+`;
