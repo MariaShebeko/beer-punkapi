af44c2fc81561719c8e47c0d0d1959ecd8630e21
MariaShebeko <marygro89@gmail.com>
2022-07-31 14:59:09 +0200

    added styles to pagination buttons

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,12 +1,48 @@
+import styled from 'styled-components';
+import { btnCl, accentTextCl, accentBgCl, textCl } from 'styles/styleVars';
+
 export default function Pagination({ prev, next, page, totalPages }) {
   return (
-    <div>
-      <button type="button" onClick={prev} disabled={page <= 1}>
+    <ButtonsWrapper>
+      <Button type="button" onClick={prev} disabled={page <= 1}>
         prev
-      </button>
-      <button type="button" onClick={next} disabled={page >= totalPages}>
+      </Button>
+      <Button type="button" onClick={next} disabled={page >= totalPages}>
         next
-      </button>
-    </div>
+      </Button>
+    </ButtonsWrapper>
   );
 }
+
+const ButtonsWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+`;
+const Button = styled.button`
+  font-size: 25px;
+  padding: 10px;
+  border-radius: 5px;
+  background-color: ${btnCl};
+  cursor: pointer;
+  border: none;
+  outline: none;
+  color: ${accentTextCl};
+  text-transform: uppercase;
+  transition: background-color 300ms cubic-bezier(0.4, 0, 0.2, 1),
+    color 300ms cubic-bezier(0.4, 0, 0.2, 1);
+
+  &:not(:last-child) {
+    margin-right: 10px;
+  }
+
+  &:hover {
+    background-color: ${textCl};
+    color: ${accentBgCl};
+  }
+
+  &[disabled] {
+    pointer-events: none;
+    opacity: 0.4;
+  }
+`;
