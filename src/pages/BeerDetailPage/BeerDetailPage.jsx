9ff0a28e0f75061f830076a50f206ae69d1cdd26
MariaShebeko <marygro89@gmail.com>
2022-07-31 11:02:16 +0200

    fixed go back button with useNavigate

--- src/pages/BeerDetailPage/BeerDetailPage.jsx
+++ src/pages/BeerDetailPage/BeerDetailPage.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
-import { useLocation, useParams, Link } from 'react-router-dom';
+import { useState, useEffect, useRef } from 'react';
+import { useLocation, useParams, Link, useNavigate } from 'react-router-dom';
 import api from '../../services/punkapi';
 import Container from '../../components/Container';
 import BeerDetails from '../../components/BeerDetails';
@@ -7,7 +7,9 @@ import BeerDetails from '../../components/BeerDetails';
 export default function BeerDetailPage() {
   const location = useLocation();
   const params = useParams();
-
+  let navigate = useNavigate();
+  const locationRef = useRef(location);
+  // console.log('locationRef', locationRef); // state = null?????
   const beerId = Number(params.beerId);
 
   // console.log('location details', location);
@@ -23,18 +25,20 @@ export default function BeerDetailPage() {
     asyncFetch();
   }, [beerId]);
 
+  const onGoBack = () => {
+    if (locationRef.current.state) {
+      const { pathname, search } = locationRef.current.state.from;
+      navigate(search ? pathname + search : pathname, { replace: true });
+    }
+    navigate(location?.state?.from ?? '/');
+  };
+
   return (
     <>
-      {/* Нормально ли так хардкодить кнопку назад, до этого использовала хук useHistory */}
       <Container>
-        <Link
-          to={{
-            pathname: `/`,
-            state: { from: location },
-          }}
-        >
-          <button>Go back</button>
-        </Link>
+        <button type="button" onClick={onGoBack}>
+          Go back
+        </button>
         {beer && <BeerDetails beer={beer} />}
       </Container>
     </>
