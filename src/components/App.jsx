cd8f5b06ae0a8c31ef7c44a3030e1da6e380e294
MariaShebeko <marygro89@gmail.com>
2022-08-04 12:31:17 +0200

    added lazy loading for pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,43 @@
-import { GlobalStyle } from './GlobalStyle';
 import { Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
 import Navigation from './Navigation';
-import {
-  HomePage,
-  BeersPage,
-  BeerDetailPage,
-  NotFoundPage,
-  FavouriteBeersPage,
-} from '../pages';
+import { GlobalStyle } from './GlobalStyle';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
+const HomePage = lazy(() =>
+  import('../pages/HomePage' /*webpackChunkName: "home-page"*/)
+);
+const BeersPage = lazy(() =>
+  import('../pages/BeersPage' /*webpackChunkName: "beers-page"*/)
+);
+const BeerDetailPage = lazy(() =>
+  import('../pages/BeerDetailPage' /*webpackChunkName: "beer-detail-page"*/)
+);
+const NotFoundPage = lazy(() =>
+  import('../pages/NotFoundPage' /*webpackChunkName: "notfound-page"*/)
+);
+const FavouriteBeersPage = lazy(() =>
+  import('../pages/FavouriteBeersPage' /*webpackChunkName: "favourite-page"*/)
+);
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Navigation />
-      <Routes>
-        <Route path="/" element={<HomePage />}></Route>
-        <Route path="/beers" element={<BeersPage />}></Route>
-        <Route path="/beers/:beerId" element={<BeerDetailPage />}></Route>
-        <Route path="/beers/favourite" element={<FavouriteBeersPage />}></Route>
-
-        <Route element={<NotFoundPage />}></Route>
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/beers" element={<BeersPage />}></Route>
+          <Route path="/beers/:beerId" element={<BeerDetailPage />}></Route>
+          <Route
+            path="/beers/favourite"
+            element={<FavouriteBeersPage />}
+          ></Route>
+          <Route element={<NotFoundPage />}></Route>
+        </Routes>
+      </Suspense>
       <ToastContainer />
     </>
   );
